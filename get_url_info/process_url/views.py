ab07ccbc983ee10a42a40ea23e32489479c53678
sidx <sidharth.xtb@gmail.com>
2016-06-18 20:07:40 +0530

    removed development code

--- get_url_info/process_url/views.py
+++ get_url_info/process_url/views.py
@@ -12,13 +12,9 @@ def getUrlData(request):
 	import json
 	url = request.POST['url']
 	response_data = getSiteData(url)
-	#return render(request, 'urlDetails.html',{})
-	print response_data
-	#printUrlData(response_data)
 	return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def printUrlData(data):
-	print 'here'
 	return render_to_response('urlDetails.html', data)
 
 def getSiteData(url):
@@ -27,7 +23,6 @@ def getSiteData(url):
 	import urllib2
 	
 	scheme, netloc, path, params, query, fragment = urlparse(url)
-	#print netloc
 	if not netloc:
 		netloc, path = path, ''
 	if scheme == '':
@@ -40,15 +35,10 @@ def getSiteData(url):
 	siteSoup = BeautifulSoup(siteData.read(), "lxml")
 	pageHead = siteSoup.head
 	pageTitle = siteSoup.title
-	#print pageHead
-	#print "-------------------------------------"
-	#print pageTitle
 	imgTagList = siteSoup.select('img[src]')
 	imgLinks = []
 	for img in imgTagList:
 		imgUrl = originalUrl + img['src']
 		if imgUrl not in imgLinks:
 			imgLinks.append(imgUrl)
-	#print imgLinks
-	#print siteData
 	return dict(url=url,originalUrl=originalUrl,title=pageTitle.string, imgList = imgLinks)
